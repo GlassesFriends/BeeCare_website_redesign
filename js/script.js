b374b4d1c3453818be7a8fc8bf840129575df2a4
iscRamirezAbril <isc.ramirez_abril@outlook.com>
2024-09-10 14:13:15 -0700

    Navbar & Header sections added.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
-// Add custom JavaScript here
+// |-----| Scroll Effect function |-----| //
+function userScroll() {
+    const navbar = document.querySelector('.navbar');
+
+    if (navbar) { 
+        const handleScroll = () => {
+            if (window.scrollY > 0) {
+                navbar.classList.add('navbar-sticky');
+            } else {
+                navbar.classList.remove('navbar-sticky');
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        handleScroll();
+    }
+}
+// |-----| Scroll Effect function |-----| //
+
+document.addEventListener('DOMContentLoaded', userScroll);
